ae824db4bd0bdc282d27ddb8e48b1ef38e896e3b
Troy Nunnally <troynunnally@brainrainsolutions.com>
2015-08-10 18:28:54 -0400

    Add sliding bar

--- Eboticon1.2/TabViewController.m
+++ Eboticon1.2/TabViewController.m
@@ -14,8 +14,12 @@
 #import "KeyboardTutorialViewController.h"
 #import "MoreViewController.h"
 
-@interface TabViewController ()
+@interface TabViewController ()<UITabBarDelegate>{
+        NSInteger currentIndex;
+}
 
+@property(nonatomic,strong)NSMutableDictionary * itemsStatus;
+@property(nonatomic,weak)UIView * moveView;
 @end
 
 @implementation TabViewController
@@ -78,6 +82,7 @@
                                                      image:[[UIImage imageNamed:@"tabbar-home-white"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]
                                              selectedImage:[[UIImage imageNamed:@"tabbar-home-orange"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
     homeViewController.tabBarItem.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+    homeViewController.tabBarItem.tag=0;
 
     
     shopNavController.tabBarItem =
@@ -85,12 +90,14 @@
                                   image:[[UIImage imageNamed:@"tabbar-cart-white"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]
                           selectedImage:[[UIImage imageNamed:@"tabbar-cart-orange"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
     shopNavController.tabBarItem.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+    shopNavController.tabBarItem.tag=1;
     
     whatsNewNavController.tabBarItem =
     [[UITabBarItem alloc] initWithTitle:@""
                                   image:[[UIImage imageNamed:@"tabbar-star-white"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]
                           selectedImage:[[UIImage imageNamed:@"tabbar-star-orange"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
     whatsNewNavController.tabBarItem.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+    whatsNewNavController.tabBarItem.tag=2;
     
     
     keyboardTutorialViewController.tabBarItem =
@@ -98,21 +105,81 @@
                                   image:[[UIImage imageNamed:@"tabbar-keyboard-white"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]
                           selectedImage:[[UIImage imageNamed:@"tabbar-keyboard-orange"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
     keyboardTutorialViewController.tabBarItem.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+    keyboardTutorialViewController.tabBarItem.tag=3;
     
     moreViewController.tabBarItem =
     [[UITabBarItem alloc] initWithTitle:@""
                                   image:[[UIImage imageNamed:@"tabbar-gear-white"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]
                           selectedImage:[[UIImage imageNamed:@"tabbar-gear-orange"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
     moreViewController.tabBarItem.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+    moreViewController.tabBarItem.tag=4;
     
     
     [self.tabBar setBarTintColor:[UIColor colorWithRed:37.0f/255.0f green:21.0f/255.0f blue:48.0f/255.0f alpha:0.5f]]; //
+    
+    
+
+
+    
+    NSLog(@"%f", self.tabBar.frame.size.height);
+    UIView * moveView = [[UIView alloc] initWithFrame:CGRectMake(0, self.tabBar.frame.size.height-5, self.view.frame.size.width/5, 5)];
+    moveView.backgroundColor = [UIColor colorWithRed:255.0f/255.0f green:108.0f/255.0f blue:0.0f/255.0f alpha:1.0f];
+    self.moveView = moveView;
+    [self.tabBar addSubview:moveView];
+    
+    
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+
+
+
+
+- (void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item{
+    NSLog(@"Did select item: %ld", (long)item.tag);
+    
+    CGFloat width = [UIScreen mainScreen].bounds.size.width/self.viewControllers.count;
+    static NSInteger lastIndex=0;
+    currentIndex = item.tag;
+    NSLog(@"currentIndex: %ld", (long)currentIndex);
+    
+    if (currentIndex != lastIndex) {
+        CGFloat lastPosX = width * lastIndex + width/2;
+        CGFloat nextPosX = width * currentIndex + width/2;
+        CAKeyframeAnimation * keyAnimation = [CAKeyframeAnimation animation];
+        keyAnimation.keyPath = @"position.x";
+        if (currentIndex > lastIndex) {
+            keyAnimation.values = @[@(lastPosX),@(nextPosX + 10),@(nextPosX)];
+        }else{
+            keyAnimation.values = @[@(lastPosX),@(nextPosX - 10),@(nextPosX)];
+        }
+        keyAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+        
+    
+        
+        //CGFloat red = arc4random()/(CGFloat)INT_MAX;
+        //CGFloat green = arc4random()/(CGFloat)INT_MAX;
+        //CGFloat blue = arc4random()/(CGFloat)INT_MAX;
+        
+        //UIColor * color = [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+        CABasicAnimation * basicAnimation = [CABasicAnimation animation];
+        basicAnimation.keyPath = @"backgroundColor";
+        //basicAnimation.toValue = (__bridge_transfer id)color.CGColor;
+        
+        CAAnimationGroup * animationGroup = [CAAnimationGroup animation];
+        animationGroup.animations = @[keyAnimation,basicAnimation];
+        animationGroup.duration = 1.0;
+        animationGroup.removedOnCompletion = NO;
+        animationGroup.fillMode = kCAFillModeForwards;
+        [self.moveView.layer addAnimation:animationGroup forKey:nil];
+        lastIndex = currentIndex;
+    }
 }
 
 /*
