66754dd2fda1a707d3dce4485fb9242fd0b4c450
The Atlanta Goat <inboxwingo@gmail.com>
2016-12-06 21:49:29 -0500

    Uploaded assets to the server for holiday and missing emoji for church pack.  Changed version and build numbers.

--- Eboticon1.2/ShopViewController.m
+++ Eboticon1.2/ShopViewController.m
@@ -165,8 +165,6 @@ static const int ddLogLevel = LOG_LEVEL_ERROR;
     else if([product.productIdentifier isEqualToString:@"com.eboticon.Eboticon.holidaypack1"]){
         cell.packImage.image = [UIImage imageNamed:[_packImages objectAtIndex:5]];
     }
-
-    
 }
 
 
