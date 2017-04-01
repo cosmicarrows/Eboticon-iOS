30319f2eb722c27c816120c82073b66302c4e3f4
Johnson Ejezie <johnsonejezie@gmail.com>
2017-04-01 11:13:09 +0100

    added new file

---
+++ EboticonKeyboard/Webservice.swift
@@ -0,0 +1,54 @@
+//
+//  Webservice.swift
+//  Eboticon1.2
+//
+//  Created by Johnson Ejezie on 25/03/2017.
+//  Copyright © 2017 Incling. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: Any]
+let kBaseURL = "http://api.eboticons.com/v1/"
+
+
+@objc class Webservice:NSObject {
+    
+    static func loadEboticons(endpoint: String, completion: @escaping ([EboticonGif]?) -> ()) {
+        guard let url = URL(string:kBaseURL+endpoint) else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            let json = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let jsonArray = json as? [JSONDictionary] {
+                var eboticons = [EboticonGif]()
+                for eboticonDict in jsonArray {
+                    let id = eboticonDict["id"] as? NSNumber ?? 0
+                    let gif = eboticonDict["gif"] as? String ?? ""
+                    let still = eboticonDict["still"] as? String ?? ""
+                    let name = eboticonDict["name"] as? String ?? ""
+                    let caption = eboticonDict["caption"] as? String ?? ""
+                    let mov = eboticonDict["movie"] as? String ?? ""
+                    let category = eboticonDict["category"] as? String ?? ""
+                    let skinTone = eboticonDict["skin_tone"] as? String ?? ""
+                    let pack = eboticonDict["pack"] as? String ?? ""
+                    
+                    let eboticon = EboticonGif(attributes: name, gifURL: gif, captionCategory: caption, category: category, eboticonID: id, movieURL: mov, stillURL: still, skinTone: skinTone, displayType: "", purchaseCategory: pack)
+                    guard let unwrappedEboticon = eboticon else {
+                        continue
+                    }
+                    eboticons.append(unwrappedEboticon)
+                }
+                completion(eboticons)
+                
+            }
+            }.resume()
+    }
+}
+
+
